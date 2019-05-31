d262bee690d5d51bf918e76d485e81a74269cec8
Konstantinos Bozikis <konstantinos.bozikis@yahoo.com>
2019-05-31 11:01:07 +0300

    comment out Program

--- CrowDo/Program.cs
+++ CrowDo/Program.cs
@@ -6,52 +6,52 @@ namespace CrowDo
     {
         static void Main(string[] args)
         {
-            var service = new UserService();
+            //var service = new UserService();
 
-            service.Create("george.b@gmail.com", "George", "Athens", new DateTime(1991, 1, 23));
-            service.Create("mariak@gmail.com", "Maria", "Thessaloniki", new DateTime(1951, 10, 2));
-            service.Create("nikos@gmail.com", "Nikos Koukos", "Athens", new DateTime(1971, 4, 10));
-            service.Create("dimitris@gmail.com", "dimitris", "Larissa", new DateTime(1965, 11, 3));
-            service.Create("stavros@gmail.com", "Stavros Nikolaou", "Athens", new DateTime(1998, 3, 12));
-            service.Create("matsa@gmail.com", "Nikoleta Matsablokou", "Kotinthos", new DateTime(2000, 2, 28));
-            service.Create("Xristina@gmail.com", "Xristina", "Athens", new DateTime(1975, 7, 14));
+            //service.Create("george.b@gmail.com", "George", "Athens", new DateTime(1991, 1, 23));
+            //service.Create("mariak@gmail.com", "Maria", "Thessaloniki", new DateTime(1951, 10, 2));
+            //service.Create("nikos@gmail.com", "Nikos Koukos", "Athens", new DateTime(1971, 4, 10));
+            //service.Create("dimitris@gmail.com", "dimitris", "Larissa", new DateTime(1965, 11, 3));
+            //service.Create("stavros@gmail.com", "Stavros Nikolaou", "Athens", new DateTime(1998, 3, 12));
+            //service.Create("matsa@gmail.com", "Nikoleta Matsablokou", "Kotinthos", new DateTime(2000, 2, 28));
+            //service.Create("Xristina@gmail.com", "Xristina", "Athens", new DateTime(1975, 7, 14));
 
-            var projectService = new ProjectService();
+            //var projectService = new ProjectService();
 
-            projectService.PublishProject("nikos@gmail.com", "Camera Tripod", "Arts", "A camera tripod idea"
-                , 15000m, new DateTime(2019, 05, 20), new DateTime(2019, 11, 10), 12);
-            projectService.PublishProject("stavros@gmail.com", "Food dispenser", "Food, Pets", "Clever food dispenser for dogs"
-                , 5000m, new DateTime(2018, 12, 21), new DateTime(2019, 07, 21), 16);
-            projectService.PublishProject("Xristina@gmail.com", "Music Album", "Arts, Music", "Music album"
-                , 45000m, new DateTime(2017, 06, 21), new DateTime(2018, 05, 29), 24);
+            //projectService.PublishProject("nikos@gmail.com", "Camera Tripod", "Arts", "A camera tripod idea"
+            //    , 15000m, new DateTime(2019, 05, 20), new DateTime(2019, 11, 10), 12);
+            //projectService.PublishProject("stavros@gmail.com", "Food dispenser", "Food, Pets", "Clever food dispenser for dogs"
+            //    , 5000m, new DateTime(2018, 12, 21), new DateTime(2019, 07, 21), 16);
+            //projectService.PublishProject("Xristina@gmail.com", "Music Album", "Arts, Music", "Music album"
+            //    , 45000m, new DateTime(2017, 06, 21), new DateTime(2018, 05, 29), 24);
 
-            var dashboard = new DashboardService();
+            //var dashboard = new DashboardService();
 
-            dashboard.AddRewardPackage(5, 2, "Tier 1", "Taperware", 400m);
-            dashboard.AddRewardPackage(5, 2, "Tier 2", "Plates", 1000m);
-            dashboard.AddRewardPackage(5, 2, "Tier 3", "Food dispencer", 2500m);
-            Console.WriteLine(dashboard.AddRewardPackage(5, 2, "Tier 4", "whatever", 800m).ErrorText);
-            dashboard.AddRewardPackage(7, 3, "Tier 1", "Fan Tshirt", 30m);
-            dashboard.AddRewardPackage(7, 3, "Tier 2", "CD", 50m);
-            dashboard.AddRewardPackage(7, 3, "Tier 3", "Signed CD", 150m);
-            dashboard.AddRewardPackage(7, 3, "Tier 4", "Signed Vinyl", 250m);
+            //dashboard.AddRewardPackage(5, 2, "Tier 1", "Dog tag", 40m);
+            //dashboard.AddRewardPackage(5, 2, "Tier 2", "Water Bowl", 100m);
+            //dashboard.AddRewardPackage(5, 2, "Tier 3", "Food dispencer", 2500m);
+            //dashboard.AddRewardPackage(5, 2, "Tier 4", "Stock", 5000m);
+            //dashboard.AddRewardPackage(7, 3, "Tier 1", "Fan Tshirt", 30m);
+            //dashboard.AddRewardPackage(7, 3, "Tier 2", "CD", 50m);
+            //dashboard.AddRewardPackage(7, 3, "Tier 3", "Signed CD", 150m);
+            //dashboard.AddRewardPackage(7, 3, "Tier 4", "Signed Vinyl", 250m);
 
-            projectService = new ProjectService();
+            //projectService = new ProjectService();
 
-            projectService.FundProject(1, 1, 2);
-            projectService.FundProject(2, 1, 3);
-            projectService.FundProject(4, 3, 6);
-            projectService.FundProject(5, 3, 5);
-            projectService.FundProject(7, 1, 3);
-            projectService.FundProject(6, 3, 7);
-            projectService.FundProject(6, 1, 3);
-            projectService.FundProject(6, 3, 5);
+            //projectService.FundProject(1, 1, 2);
+            //projectService.FundProject(2, 1, 3);
+            //projectService.FundProject(4, 3, 6);
+            //projectService.FundProject(5, 3, 5);
+            //projectService.FundProject(7, 1, 3);
+            //projectService.FundProject(6, 3, 7);
+            //projectService.FundProject(6, 1, 3);
+            //projectService.FundProject(6, 3, 5);
 
-            //var reportingService = new ReportingService();
+            ////var reportingService = new ReportingService();
 
-            //reportingService.MonthlyReport("monthly.xlsx");
-            //reportingService.MonthlyReport("weekly.xlsx");
-            Console.ReadLine();
+            ////reportingService.MonthlyReport("monthly.xlsx");
+            ////reportingService.MonthlyReport("weekly.xlsx");
+            //Console.ReadLine();
         }
     }
 }
